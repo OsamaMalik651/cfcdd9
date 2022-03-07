8e56c4578ea95c61aa2ca803d8fb795c758573eb
OsamaMalik651 <osamamalik124@outlook.com>
2022-03-07 15:07:14 -0700

    link fix

--- server/db/db.js
+++ server/db/db.js
@@ -7,21 +7,21 @@ dotenv.config();
 // const host = process.env.HOST;
 // const database = process.env.DATABASE;
 // const password = process.env.PASSWORD;
-const user = "postgres";
-const host = "localhost";
-const database = "messenger";
-const password = "R@zgriz086";
+// const user = "postgres";
+// const host = "localhost";
+// const database = "messenger";
+// const password = "R@zgriz086";
 
-// const db = new Sequelize(
-// process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
-//   {
-//     logging: false,
-//   }
-// );
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+  {
+    logging: false,
+  }
+);
 
-const db = new Sequelize(process.env.DATABASE_URL || database, user, password, {
-  host,
-  dialect: "postgres",
-  logging: false,
-});
+// const db = new Sequelize(database, user, password, {
+//   host,
+//   dialect: "postgres",
+//   logging: false,
+// });
 module.exports = db;
