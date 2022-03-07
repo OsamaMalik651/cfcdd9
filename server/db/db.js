4913f041907076cab0abfc06ee0e1afb74a1ce22
OsamaMalik651 <osamamalik124@outlook.com>
2022-03-07 14:52:50 -0700

    db url fixes to run with git actions test

--- server/db/db.js
+++ server/db/db.js
@@ -1,20 +1,25 @@
 const Sequelize = require("sequelize");
+
 const dotenv = require("dotenv");
 dotenv.config();
 
-const user = process.env.USER;
-const host = process.env.HOST;
-const database = process.env.DATABASE;
-const password = process.env.PASSWORD;
+// const user = process.env.USER;
+// const host = process.env.HOST;
+// const database = process.env.DATABASE;
+// const password = process.env.PASSWORD;
+const user = "postgres";
+const host = "localhost";
+const database = "messenger";
+const password = "R@zgriz086";
 
 // const db = new Sequelize(
-//   process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+// process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
 //   {
 //     logging: false,
 //   }
 // );
 
-const db = new Sequelize(database, user, password, {
+const db = new Sequelize(process.env.DATABASE_URL || database, user, password, {
   host,
   dialect: "postgres",
   logging: false,
