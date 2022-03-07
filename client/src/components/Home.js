dc020facfc510d16190c90b4730f1dab2d7903ca
OsamaMalik651 <osamamalik124@outlook.com>
2022-03-07 14:22:57 -0700

    issue #1 fix.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -62,16 +62,15 @@ const Home = ({ user, logout }) => {
     });
   };
 
-  const postMessage = (body) => {
+  const postMessage = async (body) => {
     try {
-      const data = saveMessage(body);
+      const data = await saveMessage(body);
 
       if (!body.conversationId) {
         addNewConvo(body.recipientId, data.message);
       } else {
         addMessageToConversation(data);
       }
-
       sendMessage(data, body);
     } catch (error) {
       console.error(error);
@@ -80,14 +79,15 @@ const Home = ({ user, logout }) => {
 
   const addNewConvo = useCallback(
     (recipientId, message) => {
-      conversations.forEach((convo) => {
+      const newConversations = [...conversations];
+      newConversations.forEach((convo) => {
         if (convo.otherUser.id === recipientId) {
           convo.messages.push(message);
           convo.latestMessageText = message.text;
           convo.id = message.conversationId;
         }
       });
-      setConversations(conversations);
+      setConversations(newConversations);
     },
     [setConversations, conversations]
   );
@@ -106,13 +106,14 @@ const Home = ({ user, logout }) => {
         setConversations((prev) => [newConvo, ...prev]);
       }
 
-      conversations.forEach((convo) => {
+      const newConversations = [...conversations];
+      newConversations.forEach((convo) => {
         if (convo.id === message.conversationId) {
           convo.messages.push(message);
           convo.latestMessageText = message.text;
         }
       });
-      setConversations(conversations);
+      setConversations(newConversations);
     },
     [setConversations, conversations]
   );
--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,22 @@
 const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const user = process.env.USER;
+const host = process.env.HOST;
+const database = process.env.DATABASE;
+const password = process.env.PASSWORD;
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+//   {
+//     logging: false,
+//   }
+// );
+
+const db = new Sequelize(database, user, password, {
+  host,
+  dialect: "postgres",
+  logging: false,
 });
-
 module.exports = db;
